193226ccba5e07573ad79ed427aacecffc0602a0
namori3 <namori3@outlook.com>
2017-05-29 11:19:59 +0900

    DEL USELESS FILE

--- LoveProject/src/namori/love/code/SetChaImg.java
+++ LoveProject/src/namori/love/code/SetChaImg.java
@@ -1,10 +1,10 @@
 package namori.love.code;
 
 import java.awt.Graphics;
-import java.awt.Graphics2D;
+//import java.awt.Graphics2D;
 import java.awt.Image;
-import java.awt.image.BufferedImage;
-import java.awt.image.RenderedImage;
+//import java.awt.image.BufferedImage;
+//import java.awt.image.RenderedImage;
 import java.io.File;
 import java.io.IOException;
 
@@ -18,7 +18,7 @@ public class SetChaImg extends JPanel {
 	public SetChaImg(String filename) throws IOException
 	{
 		img = ImageIO.read(new File(filename));
-		getImage(img);
+		//getImage(img);
 	}
 	
 	@Override
@@ -28,16 +28,16 @@ public class SetChaImg extends JPanel {
 		
 	}
 	
-	public RenderedImage getImage(Image img)
-	{
-		BufferedImage bufferedImage = new BufferedImage(img.getWidth(null),
-				img.getHeight(null), BufferedImage.TYPE_INT_RGB);
-		
-		final Graphics2D g2 = bufferedImage.createGraphics();  
-	      g2.drawImage(img, 0, 0, null);  
-	      g2.dispose();  
-	      return bufferedImage;
-	}
+//	public RenderedImage getImage(Image img)
+//	{
+//		BufferedImage bufferedImage = new BufferedImage(img.getWidth(null),
+//				img.getHeight(null), BufferedImage.TYPE_INT_RGB);
+//		
+//		final Graphics2D g2 = bufferedImage.createGraphics();  
+//	      g2.drawImage(img, 0, 0, null);  
+//	      g2.dispose();  
+//	      return bufferedImage;
+//	}
 	
 	public Image getImg()
 	{
