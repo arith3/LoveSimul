984cbc7d8d60d959c5efd03758dd2cdf2e8f59a3
namori3 <namori3@outlook.com>
2017-06-10 21:22:59 +0900

    add sth new class?

--- LoveProject/src/namori/love/code/SsibalChange.java
+++ LoveProject/src/namori/love/code/SsibalChange.java
@@ -1,5 +1,20 @@
 package namori.love.code;
 
+import javax.swing.JPanel;
+
 public class SsibalChange {
 
+	protected static JPanel temp = new JPanel();
+	
+	protected JPanel change(JPanel pn)
+	{
+//		SsibalMotherless.frame.remove(mainPane);
+//		SsibalExam ex = new SsibalExam(1);
+//		SsibalMotherless.frame.add(ex.getMain());
+//		SsibalMotherless.frame.repaint();
+		
+		
+		return temp;
+	}
+	
 }
