c8c4e694edad542737ad7febd00cddeefaedcae8
namori3 <namori3@outlook.com>
2017-06-10 19:57:14 +0900

    card panel testing...

--- LoveProject/src/namori/love/code/CardPanel.java
+++ LoveProject/src/namori/love/code/CardPanel.java
@@ -1,17 +1,63 @@
 package namori.love.code;
 
+import java.awt.CardLayout;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
+
 import javax.swing.JPanel;
 
-public class CardPanel extends JPanel {
+public class CardPanel implements KeyListener{
 
-	private static final long serialVersionUID = 1L;
+	private CardLayout card;
+	private FrameLayout2 fr2;
+	private ArrayList<ExamPan> eparr;
+	private JPanel cardPan;
 
-	public CardPanel() {
+	protected CardPanel() {
 
+		card = new CardLayout();
+		cardPan = new JPanel(card);
+		cardPan.addKeyListener(this);
+		//cardPan.setLayout(card);]
+		cardPan.setBounds(0, 0, 1280, 720);
+		cardPan.setVisible(true);
+		fr2 = new FrameLayout2();
+		fr2.setBounds(0, 0, 1280, 720);
+		cardPan.add(fr2.getMain(), "talk");
+		eparr = new ArrayList<ExamPan>();
+		for (int i = 0; i < 2; i++) {
+			eparr.add(i, new ExamPan(i+1));
+			cardPan.add(eparr.get(i).getMain(), "ex"+i);
+		} //일단 4번 문제까지 생성함
+		
+		cardPan.add(fr2.getMain(), "talk");
+			
 	}
 
-	
-	
-	
-	
+	protected JPanel getCard() {
+		return cardPan;
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		char c = e.getKeyChar();
+		System.out.println("CARD CHANGE??");
+		if(c == 'c')
+			card.show(cardPan, "ex2");
+	}
+
 }
---
+++ LoveProject/src/namori/love/code/CardTest.java
@@ -0,0 +1,24 @@
+package namori.love.code;
+
+import javax.swing.JFrame;
+
+public class CardTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		JFrame frame = new JFrame("¢¾ Love Simulator ¢¾");
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(Stat.FrameWidth, Stat.FrameHeight);
+		frame.setResizable(false);
+		frame.setLocation(100, 100);
+		frame.setVisible(true);
+		
+		CardPanel cp = new CardPanel();
+		
+		frame.add(cp.getCard());
+		
+	}
+
+}
